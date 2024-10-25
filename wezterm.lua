d4459cc67f6f53c834299332b1198fee950ebd06
hhheath <heath@steppe.sh>
2024-10-25 12:49:03 -0500

    update wezterm config for easier split pane keys and ensure cmd+w closes panes instead of the whole tab

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,36 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.default_cursor_style = "SteadyBar"
+
 config.font = wezterm.font("BlexMono Nerd Font")
 config.font_size = 14.0
 
 config.color_scheme = "Catppuccin Frappe"
+-- config.color_scheme = "iTerm2 Solarized Dark"
 
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.initial_cols = 175
 config.initial_rows = 50
+
+config.keys = {
+	-- open split pane vertically
+	{
+		key = "-",
+		mods = "SUPER|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "|",
+		mods = "SUPER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+}
 
 return config
