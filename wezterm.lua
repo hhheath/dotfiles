282504a0c5278344b125de049cc2b242a0c4eb45
Heath Blandford <35548666+hhheath@users.noreply.github.com>
2024-11-12 07:46:12 -0600

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,11 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-
-config.default_cursor_style = "SteadyBar"
+-- config.default_cursor_style = "SteadyBar"
 
 config.font = wezterm.font("BlexMono Nerd Font")
 config.font_size = 14.0
 
-config.color_scheme = "Catppuccin Frappe"
+config.color_scheme = "Catppuccin Mocha"
 
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
@@ -14,12 +13,23 @@ config.initial_cols = 175
 config.initial_rows = 50
 
 config.keys = {
+	{
+		key = "t",
+		mods = "SUPER",
+		action = wezterm.action.SpawnCommandInNewTab({ cwd = wezterm.home_dir }),
+	},
+	{
+		key = "t",
+		mods = "SUPER|SHIFT",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
 	-- open split pane vertically
 	{
 		key = "-",
 		mods = "SUPER|SHIFT",
 		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
 	},
+	-- open split plane horizontally
 	{
 		key = "|",
 		mods = "SUPER|SHIFT",
@@ -33,3 +43,4 @@ config.keys = {
 }
 
 return config
+
