8471aa92599d19dbd8118a4b2e3e6b4cbaf2f07e
hhheath <heath@steppe.sh>
2024-11-13 18:48:59 -0600

    update wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
--- config.default_cursor_style = "SteadyBar"
+config.default_cursor_style = "SteadyBar"
 
 config.font = wezterm.font("BlexMono Nerd Font")
 config.font_size = 14.0
@@ -43,4 +43,3 @@ config.keys = {
 }
 
 return config
-
